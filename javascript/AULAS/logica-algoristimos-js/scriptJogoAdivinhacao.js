d38f1c468ec20f93ed71e5a3f9f8b59ba0673e65
Jair Torezone <jairtorezone@gmail.com>
2023-08-17 21:49:10 -0300

    Dom - jogo adivinhação lógica

--- javascript/AULAS/logica-algoristimos-js/scriptJogoAdivinhacao.js
+++ javascript/AULAS/logica-algoristimos-js/scriptJogoAdivinhacao.js
@@ -17,9 +17,6 @@
 */
 
 let result = prompt("Advinhe o número que estou pensando? Está entre 0 e 10");
-const randomNumber = Math.round(Math.random() * 10);
-
-let xAttempts = 1;
 
 while (Number(result) != randomNumber) {
   result = prompt("Erro, tente novamente:");
