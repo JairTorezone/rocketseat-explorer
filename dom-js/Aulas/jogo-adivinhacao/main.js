06ab203ad119ddcbc068127a771e2da7b6520f42
Jair Torezone <jairtorezone@gmail.com>
2023-08-18 09:28:03 -0300

    DOM - jogo adivinhação addEventListener

--- dom-js/Aulas/jogo-adivinhacao/main.js
+++ dom-js/Aulas/jogo-adivinhacao/main.js
@@ -1,10 +1,13 @@
 const randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
 
-function handleClick(event) {
+const button = document.querySelector("button");
+
+button.addEventListener("click", function (event) {
   event.preventDefault();
 
   const inputNumber = document.querySelector("#inputNumber");
+  console.log("Valor do input = " + inputNumber.value);
 
   if (Number(inputNumber.value) == randomNumber) {
     document.querySelector(".screen1").classList.add("hide");
@@ -15,5 +18,27 @@ function handleClick(event) {
     ).innerText = `acertou em ${xAttempts} tentativas`;
   }
 
+  console.log("Tentativas " + xAttempts);
   xAttempts++;
-}
+});
+
+/**function normal */
+
+// function handleClick(event) {
+//   event.preventDefault();
+
+//   const inputNumber = document.querySelector("#inputNumber");
+//   console.log("Valor do input = " + inputNumber.value);
+
+//   if (Number(inputNumber.value) == randomNumber) {
+//     document.querySelector(".screen1").classList.add("hide");
+//     document.querySelector(".screen2").classList.remove("hide");
+
+//     document.querySelector(
+//       ".screen2 h2"
+//     ).innerText = `acertou em ${xAttempts} tentativas`;
+//   }
+
+//   console.log("Tentativas " + xAttempts);
+//   xAttempts++;
+// }
