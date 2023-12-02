9defeaf13c43127b9f916cb41f17fc3001215453
Jair Torezone <jairtorezone@gmail.com>
2023-12-02 09:35:35 -0300

    ReactJs - introdução, Vite, JSX, Fragment

--- node/aulas/api/src/controllers/NotesController.js
+++ node/aulas/api/src/controllers/NotesController.js
@@ -58,32 +58,6 @@ class NotesController {
     return response.json();
   }
 
-  // async index(request, response) {
-  //   const { title, user_id, tags } = request.query;
-
-  //   let notes;
-
-  //   if (tags) {
-  //     const filterTags = tags.split(",").map((tag) => tag.trim());
-
-  //     notes = await knex("tags")
-  //       .select(["notes.id", "notes.title", "notes.user_id"])
-  //       .where("notes.user_id", user_id)
-  //       .whereLike("notes.title", `%${title}%`)
-  //       .whereIn("name", filterTags)
-  //       .innerJoin("notes", "notes.id", "tags.note.id")
-  //       .orderBy("notes.title");
-
-  //     //----
-  //   } else {
-  //     notes = await knex("notes")
-  //       .where({ user_id })
-  //       .whereLike("title", `%${title}%`)
-  //       .orderBy("title");
-  //   }
-
-  //   return response.json({ notes });
-  // }
   async index(request, response) {
     const { title, user_id, tags } = request.query;
 
