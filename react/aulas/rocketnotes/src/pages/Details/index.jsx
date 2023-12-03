b569cc99a6bbdf79118a725496a530b8fe6af176
Jair Torezone <jairtorezone@gmail.com>
2023-12-03 14:26:06 -0300

    ReactJs - add button component

--- react/aulas/rocketnotes/src/pages/Details/index.jsx
+++ react/aulas/rocketnotes/src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
 import { Container } from "./style";
+import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
       <h1>Hello world BRAZIL!</h1>
       <span>Torezone</span>
+      <Button />
     </Container>
   );
 }
