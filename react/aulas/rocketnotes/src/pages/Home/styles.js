11f39820d25ed2f9ea0d9c2562c4b2097fab8250
Jair Torezone <jairtorezone@gmail.com>
2023-12-08 11:46:52 -0300

    ReactJs - created logotipo Home

--- react/aulas/rocketnotes/src/pages/Home/styles.js
+++ react/aulas/rocketnotes/src/pages/Home/styles.js
@@ -18,25 +18,33 @@ export const Container = styled.div`
 
 export const Brand = styled.div`
   grid-area: brand;
-  background: red;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+
+  > h1 {
+    font-size: 24px;
+    color: ${({ theme }) => theme.COLORS.ORANGE};
+  }
 `;
 
 export const Menu = styled.ul`
   grid-area: menu;
-  background: blue;
 `;
 
 export const Search = styled.div`
   grid-area: search;
-  background: gray;
 `;
 
 export const Content = styled.div`
   grid-area: content;
-  background: green;
 `;
 
 export const NewNote = styled.button`
   grid-area: newnote;
-  background: yellow;
 `;
