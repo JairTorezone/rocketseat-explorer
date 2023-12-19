f0a3f41c8069a765952c549f34f61ea72167195e
Jair Torezone <jairtorezone@gmail.com>
2023-12-18 22:20:14 -0300

    ReactJs - Estilizando o NoteItem

--- react/aulas/rocketnotes/src/pages/New/index.jsx
+++ react/aulas/rocketnotes/src/pages/New/index.jsx
@@ -1,6 +1,8 @@
 import { Header } from "../../components/Header";
 import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
+import { NoteItem } from "../../components/NoteItem";
+import { Section } from "../../components/Section";
 
 import { Container, Form } from "./styles";
 
@@ -8,16 +10,22 @@ export function New() {
   return (
     <Container>
       <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
 
-      <Form>
-        <header>
-          <h1>Criar nota</h1>
-          <a href="/">voltar</a>
-        </header>
+          <Input placeholder="Título" />
+          <Textarea placeholder="observações" />
 
-        <Input placeholder="Título" />
-        <Textarea placeholder="observações" />
-      </Form>
+          <Section title="Links úteis">
+            <NoteItem value="https://rocktseat.com.br" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+        </Form>
+      </main>
     </Container>
   );
 }
