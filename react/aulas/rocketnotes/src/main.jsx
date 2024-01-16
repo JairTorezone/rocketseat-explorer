32278fa35daf98c3bcc145eb2ba2d1cd543f9ecc
Jair Torezone <jairtorezone@gmail.com>
2024-01-16 20:27:55 -0300

    Nodejs - useContext, MyContext

--- react/aulas/rocketnotes/src/main.jsx
+++ react/aulas/rocketnotes/src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
+import { ThemeProvider } from "styled-components";
+
+import { MyContext } from "./myContext";
 
 import theme from "./styles/theme";
 
@@ -11,7 +13,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+      <MyContext.Provider
+        value={{ name: "Jair Torezone", email: "jairtorezone@gmail.com" }}
+      >
+        <Routes />
+      </MyContext.Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
