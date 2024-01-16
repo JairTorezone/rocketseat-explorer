25bf8caa6f983b9e3f41f625a5bcd8ab5fbe0ad6
Jair Torezone <jairtorezone@gmail.com>
2024-01-16 19:28:10 -0300

    Nodejs - upload image, DiskStorage

--- node/aulas/api/src/providers/DiskStorage.js
+++ node/aulas/api/src/providers/DiskStorage.js
@@ -23,3 +23,5 @@ class DiskStorage {
     await fs.promises.unlink(filePath);
   }
 }
+
+module.exports = DiskStorage;
--- node/aulas/api/src/routes/users.routes.js
+++ node/aulas/api/src/routes/users.routes.js
@@ -1,13 +1,28 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../config/upload");
 
 const UsersControllers = require("../controllers/UsersControllers");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 const usersControllers = new UsersControllers();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersControllers.create);
 usersRoutes.put("/", ensureAuthenticated, usersControllers.update);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  userAvatarController.update
+  // (request, response) => {
+  //   console.log(request.file.filename);
+  //   response.json();
+  // }
+);
 
 module.exports = usersRoutes;
