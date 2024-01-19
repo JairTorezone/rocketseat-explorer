76bb99b6a522c4437392ccba2e03b67704b16314
Jair Torezone <jairtorezone@gmail.com>
2024-01-19 20:51:49 -0300

    Nodejs - added and removing tags

--- react/aulas/rocketnotes/src/pages/New/index.jsx
+++ react/aulas/rocketnotes/src/pages/New/index.jsx
@@ -13,6 +13,9 @@ export function New() {
   const [links, setLinks] = useState([]);
   const [newLink, setNewLink] = useState("");
 
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
   function handleAddLink() {
     setLinks((prevState) => [...prevState, newLink]);
     setNewLink("");
@@ -20,6 +23,14 @@ export function New() {
 
   function handleRemoveLink(deleted) {
     setLinks((prevState) => prevState.filter((link) => link !== deleted));
+  }
+
+  function handleAddTag() {
+    setTags((prevState) => [...prevState, newTag]);
+    setNewTag("");
+  }
+  function handleRemoveTag(deleted) {
+    setTags((prevState) => prevState.filter((tag) => tag !== deleted));
   }
 
   return (
@@ -54,8 +65,21 @@ export function New() {
 
           <Section title="Marcadores">
             <div className="tags">
-              <NoteItem value="react" />
-              <NoteItem isNew placeholder="Novo tag" />
+              {tags.map((tag, index) => (
+                <NoteItem
+                  key={String(index)}
+                  value={tag}
+                  onClick={() => handleRemoveTag(tag)}
+                />
+              ))}
+
+              <NoteItem
+                isNew
+                placeholder="Novo tag"
+                onChange={(e) => setNewTag(e.target.value)}
+                value={newTag}
+                onClick={handleAddTag}
+              />
             </div>
           </Section>
 
