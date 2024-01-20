aabef85bbb90137885c822b39d9de0c6fe18c1ab
Jair Torezone <jairtorezone@gmail.com>
2024-01-20 12:29:16 -0300

    Nodejs - filter notes

--- react/aulas/rocketnotes/src/pages/Home/index.jsx
+++ react/aulas/rocketnotes/src/pages/Home/index.jsx
@@ -11,7 +11,25 @@ import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 
 export function Home() {
+  const [search, setSearch] = useState("");
   const [tags, setTags] = useState([]);
+  const [tagsSelected, setTagsSelected] = useState([]);
+  const [notes, setNotes] = useState([]);
+
+  function handleTagSelected(tagName) {
+    if (tagName === "all") {
+      return setTagsSelected([]);
+    }
+
+    const alreadySelected = tagsSelected.includes(tagName);
+
+    if (alreadySelected) {
+      const filteredTags = tagsSelected.filter((tag) => tag !== tagName);
+      setTagsSelected(filteredTags);
+    } else {
+      setTagsSelected((prevState) => [...prevState, tagName]);
+    }
+  }
 
   useEffect(() => {
     async function fetTags() {
@@ -21,6 +39,17 @@ export function Home() {
 
     fetTags();
   }, []);
+
+  useEffect(() => {
+    async function fetcNotes() {
+      const response = await api.get(
+        `/notes?title=${search}&tags=${tagsSelected}`
+      );
+      setNotes(response.data);
+    }
+
+    fetcNotes();
+  }, [tagsSelected, search]);
 
   return (
     <Container>
@@ -32,32 +61,37 @@ export function Home() {
 
       <Menu>
         <li>
-          <ButtonText title="Todos" isactive />
+          <ButtonText
+            title="Todos"
+            onClick={() => handleTagSelected("all")}
+            isactive={tagsSelected.length === 0}
+          />
         </li>
 
         {tags &&
           tags.map((tag) => (
             <li key={String(tag.id)}>
-              <ButtonText title={tag.name} />
+              <ButtonText
+                title={tag.name}
+                onClick={() => handleTagSelected(tag.name)}
+                isactive={tagsSelected.includes(tag.name)}
+              />
             </li>
           ))}
       </Menu>
 
       <Search>
-        <Input placeholder="Pesquisar pelo título" />
+        <Input
+          placeholder="Pesquisar pelo título"
+          onChange={() => setSearch(e.target.value)}
+        />
       </Search>
 
       <Content>
         <Section title="Minhas notas">
-          <Note
-            data={{
-              title: "React",
-              tags: [
-                { id: "1", name: "react" },
-                { id: "2", name: "rocktseat" },
-              ],
-            }}
-          />
+          {notes.map((note) => (
+            <Note key={String(note.id)} data={note} />
+          ))}
         </Section>
       </Content>
 
