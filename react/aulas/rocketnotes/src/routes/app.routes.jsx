14e89c0a15d9d7a3094742b7f6ac1ab43a3ef353
Jair Torezone <jairtorezone@gmail.com>
2024-01-22 19:16:53 -0300

    Nodejs - Handling Unavailable Routes

--- react/aulas/rocketnotes/src/routes/app.routes.jsx
+++ react/aulas/rocketnotes/src/routes/app.routes.jsx
@@ -1,22 +1,4 @@
-// import { Routes, Route } from "react-router-dom";
-
-// import { New } from "../pages/New";
-// import { Home } from "../pages/Home";
-// import { Details } from "../pages/Details";
-// import { Profile } from "../pages/Profile";
-
-// export function AppRoutes() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/new" element={<New />} />
-//       <Route path="/profile" element={<Profile />} />
-//       <Route path="/details/:id" element={<Details />} />
-//     </Routes>
-//   );
-// }
-
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import { Profile } from "../pages/Profile";
 import { Details } from "../pages/Details";
@@ -30,6 +12,8 @@ export function AppRoutes() {
       <Route path="/details/:id" element={<Details />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/new" element={<New />} />
+
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
